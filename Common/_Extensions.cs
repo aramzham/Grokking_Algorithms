bb8d010c6c5b55377f22deb995fa7d0c05a83835
Aram Zhamkochyan <aramzham@hotmail.com>
2021-04-03 20:24:26 +0400

    sum by recursion added

--- Common/_Extensions.cs
+++ Common/_Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Common
@@ -18,6 +19,13 @@ namespace Common
             sb.Remove(sb.Length - 1, 1);
             sb.Append(']');
             return sb.ToString();
+        }
+
+        public static T[] SubArray<T>(this T[] array, int offset, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(array, offset, result, 0, length);
+            return result;
         }
     }
 }
---
+++ SumByRecursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Common;
+
+namespace SumByRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var array = RandomGenerator.GetRandomArray(0, 10, 5);
+                var sum = Sum(array);
+                Console.WriteLine($"{array.Stringify()}.Sum() = {sum}"); 
+                Thread.Sleep(3000);
+            }
+        }
+
+        public static int Sum(int[] array)
+        {
+            if (array.Length == 0)
+                return 0;
+
+            var firstElement = array[0];
+            return firstElement + Sum(array.SubArray(1, array.Length - 1));
+        }
+    }
+}
