75a2743b740df3705cb9838f06347b43b146111f
Aram Zhamkochyan <aramzham@hotmail.com>
2021-04-07 00:34:04 +0400

    added getHash method

--- CustomHashTable/MyHashTable.cs
+++ CustomHashTable/MyHashTable.cs
@@ -1,21 +1,42 @@
 ﻿namespace CustomHashTable
 {
-    public class MyHashTable<TKey, TValue>
+    public class MyHashTable
     {
         // load factor > 0.7 => resize by twice
         private int[] _baseArray = new int[4]; // we start by 4
+        private Slot[] _slots = new Slot[4];
         private int _occupiedSlotsCount;
         private float _loadFactor => (float)_occupiedSlotsCount / _baseArray.Length;
         private const float RESIZE_THRESHOLD = 0.7f;
 
-        public void Add(TKey key, TValue value)
+        public void Add(string key, int value)
         {
+            var baseArrayIndex = GetBaseArrayIndex(key);
+            AddToSlot(baseArrayIndex, value);
+        }
+
+        public int Get(string key)
+        {
+            var baseArrayIndex = GetBaseArrayIndex(key);
 
         }
 
-        //private int GetHash(TKey key)
-        //{
+        private void AddToSlot(int index, int value)
+        {
+            var slot = _slots[index];
+            slot ??= new Slot();
+            slot.Add(value);
+        }
 
-        //}
+        private int GetBaseArrayIndex(string key)
+        {
+            var hash = GetHash(key);
+            return hash % _baseArray.Length;
+        }
+
+        private int GetHash(string key)
+        {
+            return (int)MurmurHash2.Hash(key);
+        }
     }
 }
---
+++ CustomHashTable/Slot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CustomHashTable
+{
+    public class Slot
+    {
+        private readonly LinkedList<int> _linkedList = new LinkedList<int>();
+
+        public void Add(int item)
+        {
+            _linkedList.AddLast(item);
+        }
+    }
+}
